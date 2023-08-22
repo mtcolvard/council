f9bb182d65f3de7f047a0d5f6d1240bafe983a20
Matty Talty <matthew.colvard@gmail.com>
2023-08-22 07:00:32 +0200

    added painting assets. mobile needs to be size regular rather than skinny

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,33 +3,88 @@ import Image from 'next/image'
 import Navigation from '../components/navigation'
 import gabriellaCarving from '../../public/gabriella-carving.webp'
 
+// export default function Contact() {
+//   return (
+//     <>
+//       <Navigation />
+//       <div className="mx-4 mt-6 md:px-40  object-center">
+//         <Image
+//           src={gabriellaCarving}
+//           alt="Gabriella shaping a large sculpture's head"
+//           height="500"
+//         />
+//         <h1 className="pt-6 text-lg">Studio Berlin</h1>
+//         <div>
+//           <p>Grossgorschenstrasse 7</p>
+//           <p>10827 Berlin</p>
+//         </div>
+
+//         <h1 className="pt-6 text-lg">Studio Palermo</h1>
+//         <div>
+//           <p>Via Pacini, 113</p>
+//           <p>90143 Palermo</p>
+//         </div>
+
+//         <div className="pt-6 ">
+//           <a href="tel:+393493267015">+39 349 3267015</a>
+//         </div>
+//         <div>
+//           <a href="mailto:gabriellafurno.gf@gmail.com">gabriellafurno.gf@gmail.com</a>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
 export default function Contact() {
   return (
     <>
       <Navigation />
-      <div className="px-2.5 pt-12 md:px-8 md:pt-24 md:mx-48 object-center">
-        <Image
-          src={gabriellaCarving}
-          alt="Gabriella shaping a large sculpture's head"
-          height="500"
-        />
-        <h1 className="pt-6 text-lg">Studio Berlin</h1>
-        <div>
-          <p>Grossgorschenstrasse 7</p>
-          <p>10827 Berlin</p>
+      <div className="mx-4 mt-6 md:px-40 ">
+        <hr className="mb-6"></hr>
+        <div className="hidden sm:grid grid-cols-4">
+          <div className=" mt-6 pr-12 col-span-3">
+            <Image
+              src={gabriellaCarving}
+              alt="Gabriella shaping a large sculpture's head"
+              height="500"
+            />
+          </div>
+          <div>
+            <h1 className="mt-6 mb-3 text-lg">Studio Berlin</h1>
+            <div>
+              <p>Grossgorschenstrasse 7</p>
+              <p>10827 Berlin</p>
+            </div>
+            <h1 className="mt-12 mb-3 text-lg">Studio Palermo</h1>
+            <div>
+              <p>Via Pacini, 113</p>
+              <p>90143 Palermo</p>
+            </div>
+          </div>
+          
         </div>
+        <div className="sm:hidden">
+          <div className=" mt-6">
+            <Image
+              src={gabriellaCarving}
+              alt="Gabriella shaping a large sculpture's head"
+              height="500"
+            />
+          </div>
+          <div>
+            <h1 className="mt-12 mb-1.5 text-lg">Studio Berlin</h1>
+            <div>
+              <p>Grossgorschenstrasse 7</p>
+              <p>10827 Berlin</p>
+            </div>
+            <h1 className="mt-12 mb-1.5 text-lg">Studio Palermo</h1>
+            <div>
+              <p>Via Pacini, 113</p>
+              <p>90143 Palermo</p>
+            </div>
+          </div>
 
-        <h1 className="pt-6 text-lg">Studio Palermo</h1>
-        <div>
-          <p>Via Pacini, 113</p>
-          <p>90143 Palermo</p>
-        </div>
-
-        <div className="pt-6 ">
-          <a href="tel:+393493267015">+39 349 3267015</a>
-        </div>
-        <div>
-          <a href="mailto:gabriellafurno.gf@gmail.com">gabriellafurno.gf@gmail.com</a>
         </div>
       </div>
     </>
--- app/painting/page.js
+++ app/painting/page.js
@@ -1,13 +1,87 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import Navigation from '../components/navigation.js'
+import ImageArray from '../components/images-array.js'
+
+import painting_4 from '../../public/Webp_images/painting/painting_4.webp'
+import painting_3 from '../../public/Webp_images/painting/painting_3.webp'
+import painting_2 from '../../public/Webp_images/painting/painting_2.webp'
+import painting_1 from '../../public/Webp_images/painting/painting_1.webp'
 
 
-export default function Painting() {
+const Sculptures = () => {
+  const imageObject = [
+    {
+      link: '',
+      src: painting_4,
+      alt: '',
+      title: '',
+      details: '',
+      material: '',
+      size: '',
+      style: 'skinny',
+      width: '100%'
+    },
+    {
+      link: '',
+      src: painting_1,
+      alt: '',
+      title: '',
+      details: '',
+      material: '',
+      size: '',
+      style: 'skinny',
+      width: '100%'
+    },
+    {
+      link: '',
+      src: painting_2,
+      alt: '',
+      title: '',
+      details: '',
+      material: '',
+      size: '',
+      style: 'skinny',
+      width: '100%'
+    },
+    {
+      link: '',
+      src: painting_3,
+      alt: '',
+      title: '',
+      details: '',
+      material: '',
+      size: '',
+      style: 'skinny',
+      width: '100%'
+    },
+  ]
+
   return (
-    <>
+    <div className="bg-transparent">
       <Navigation />
-
-      <h1 className="text-4xl">Letsa get Lucky</h1>
-    </>
+      <div className="site min-h-screen flex flex-col mx-4 md:px-40  bg-white text-black">
+        {/* <div className="">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </div> */}
+        <main className="">
+          <div className="leadImageContainer">
+            <div className="featured ">
+              <ImageArray imageObject={imageObject.slice(0, 1)} />
+            </div>
+          </div>
+          <div className="content ">
+            <ImageArray imageObject={imageObject.slice(1)} />
+          </div>
+          {/* <Footer /> */}
+        </main>
+      </div>
+    </div>
   )
 }
+
+export default Sculptures
+
+// flex - [1_0_auto]
